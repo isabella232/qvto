ba17bedddbd3503f8fbaeb34bc29b1d21e53a071
cgerking <cgerking@campus.upb.de>
2015-03-13 15:51:16 +0100

    [326871] - Restore copyright

--- plugins/org.eclipse.m2m.qvt.oml/src/org/eclipse/m2m/internal/qvt/oml/blackbox/BlackboxRegistry.java
+++ plugins/org.eclipse.m2m.qvt.oml/src/org/eclipse/m2m/internal/qvt/oml/blackbox/BlackboxRegistry.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2015 Borland Software Corporation and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *   
+ * Contributors:
+ *     Borland Software Corporation - initial API and implementation
+ *     Christopher Gerking - bugs 289982, 326871, 427237
+ *******************************************************************************/
 package org.eclipse.m2m.internal.qvt.oml.blackbox;
 
 import java.util.ArrayList;
