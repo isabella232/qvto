764ea30b426d09cbaafd8b498d8505609a674285
Adolfo SBH <adolfosbh@gmail.com>
2015-10-16 11:16:41 +0100

    [397429] - ImperativeOCL grammar and CS meta-model

--- examples/org.eclipse.qvto.examples.xtext.qvtoperational.tests/src/org/eclipse/qvto/examples/xtext/qvtoperational/tests/QVToXtextTestCase.java
+++ examples/org.eclipse.qvto.examples.xtext.qvtoperational.tests/src/org/eclipse/qvto/examples/xtext/qvtoperational/tests/QVToXtextTestCase.java
@@ -98,9 +98,63 @@ public class QVToXtextTestCase extends AbstractQVToXtextTestCase {
 	public void testSimpleMappingDefinition() throws Exception {
 		String testFile =
 				"modeltype MM1 uses \"m1URI\";\n"						
-				+ " transformation t1(inout m1 : MM1) {"
+				+ " transformation t1(inout m1 : MM1) {\n"
 				+ " mapping m1 (in p1 : String);\n"
 				+ "}";
 		doLoadFromString("simpleMappingDefinition.qvto2", testFile);
+	}
+	
+	public void testAllImperativeOCL() throws Exception {
+		String testFile = 
+				"modeltype MM1 uses \"m1URI\";\n"						
+				+ " transformation t1(inout m1 : MM1) {\n"
+				+ " main() {\n"
+				+ "	  var coll : List(String) := List{};\n"
+				+ "	  do {}; -- Empty block expression\n"
+				+ "	  { {} }; -- Nested empty blocks\n"
+				+ "	  { var a := \"abc\" }\n"
+				+ "	  { var a : String := \"abc\" }\n"
+				+ "	  { var a : String := var b : String ::= \"abc\" }\n"
+				+ "	  { var a := \"abc\", b := 1}\n"
+				+ "	  { return ; } -- Return expression\n" 
+				+ "   { compute (x : String = \"abc\") { {} }; }\n"
+				+ "   { while ( x <> null) { {} }; }\n"
+				+ "   { while (x : String = \"abc\"; x <> null) { {} }; }\n"
+				+ "   { coll->forEach(i) { {} }; }\n"
+				+ "   { coll->forEach(i : String | i <> null) { {} }; }\n"
+				+ "   { coll->xselect(i : String | i <> null); }\n"
+				+ "   { coll->xselect(i | i <> null); }\n"
+				+ "   { coll->xselectOne(i : String | i <> null); }\n"
+				+ "   { coll->xselectOne(i | i <> null); }\n"
+				+ "   { coll->xcollect(i : String | i.toUpper()); }\n"
+				+ "   { coll->xcollect(i | i.toUpper()); }\n"
+				+ "   { coll->xcollect(toUpper()); }\n"
+//				+ "   { coll->xcollectselect(i : String ; res := i.toUpper() | res->notEmpty()) ; }\n"
+//				+ "   { coll->xcollectselect(i ; res := i.toUpper() | res->notEmpty()); }\n"
+//				+ "   { coll->xcollectselect(res := toUpper() | res->notEmpty()); }\n"
+//				+ "   { coll->xcollectselectOne(i : String ; res := i.toUpper() | res->notEmpty()); }\n"
+//				+ "   { coll->xcollectselectOne(i ; res := i.toUpper() | res->notEmpty()); }\n"
+//				+ "   { coll->xcollectselectOne(res := toUpper() | res->notEmpty()); }\n"
+				+ "   { if (1 = 2) false elif (2 = 3) false else true endif; }\n"
+				+ "   { if (1 = 2) false elif (2 = 3) false elif (3 = 4) false else true; }\n"
+				+ "   { coll := Sequence{\"abc\"}; }\n"
+				+ "   { coll ::= Sequence{\"abc\"}; }\n"
+				+ "   { coll += Sequence{\"abc\"}; }\n"
+				+ "   { coll := null default Sequence{\"abc\"}; }\n"
+				+ "   { afeature.unlink(anObject); }\n"
+				+ "   { try { } except (exp : Exception1, Exception2) {} }\n"
+				+ "   { try { } except (exp : Exception1, Exception2) {} except (Exception) {} }\n"
+				+ "   { raise StringException(\"problem!!!\"); }\n"
+				+ "   { log (\"Logging...\"); }\n"
+				+ "   { log (\"Logging...\", coll, warning) when coll->isEmpty(); }\n"
+				+ "   { assert coll->notEmpty(); }\n"
+				+ "   { assert coll->notEmpty() with log (\"Empty collection\", coll); }\n"
+				+ "   { assert fatal coll->notEmpty() with log (\"Empty collection\", coll); }\n"
+				+ "   { new String(); }\n"
+				+ "   { new String(\"abc\"); }\n"
+				+ "   { new EClass@MM1(); }\n"
+				+ "  }\n"
+				+ "}";
+		doLoadFromString("allImperativeOCL.qvto2", testFile);
 	}
 }
