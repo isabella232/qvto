87894897976152d41639b0ab4d8fa6cc4176c239
Ed Willink <ed@willink.me.uk>
2017-10-25 08:20:02 +0100

    [releng] Trim experimental support

--- examples/org.eclipse.qvto.examples.xtext.qvtoperational/src/org/eclipse/qvto/examples/xtext/qvtoperational/utilities/QVTOperationalCSResource.java
+++ examples/org.eclipse.qvto.examples.xtext.qvtoperational/src/org/eclipse/qvto/examples/xtext/qvtoperational/utilities/QVTOperationalCSResource.java
@@ -2,23 +2,11 @@ package org.eclipse.qvto.examples.xtext.qvtoperational.utilities;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.jdt.annotation.NonNull;
-import org.eclipse.ocl.pivot.internal.utilities.EnvironmentFactoryInternal;
-import org.eclipse.ocl.pivot.resource.ASResource;
-import org.eclipse.ocl.xtext.base.cs2as.CS2AS;
 import org.eclipse.qvto.examples.pivot.qvtoperational.QVTOperationalPackage;
 import org.eclipse.qvto.examples.xtext.imperativeocl.utilities.ImperativeOCLCSResource;
-import org.eclipse.qvto.examples.xtext.qvtoperational.cs2as.QVTOperationalCS2AS;
 
 public class QVTOperationalCSResource
-		extends ImperativeOCLCSResource {
-	
-	@Override
-	public @NonNull CS2AS createCS2AS(@NonNull EnvironmentFactoryInternal environmentFactory,
-			@NonNull ASResource asResource) {
-		return new QVTOperationalCS2AS(environmentFactory, this, asResource);
-	}
-	
-
+extends ImperativeOCLCSResource {
 
 	@Override
 	public @NonNull String getASContentType() {
