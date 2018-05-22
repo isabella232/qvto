45a82261b66aedfaf971c7360ec6501c186bec80
Ed Willink <ed@willink.me.uk>
2018-05-22 11:17:42 +0100

    [releng] Apply Copyright tool to Releng

--- releng/org.eclipse.qvto.releng.build-site/updates.sh
+++ releng/org.eclipse.qvto.releng.build-site/updates.sh
@@ -1,4 +1,14 @@
 #!/bin/bash -xv
+#*******************************************************************************
+# Copyright (c) 2017, 2018 Willink Transformations and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#     E.D.Willink - initial API and implementation
+#*******************************************************************************
 #
 #    Promote the PUBLISH__URL to an updates repository.
 #
