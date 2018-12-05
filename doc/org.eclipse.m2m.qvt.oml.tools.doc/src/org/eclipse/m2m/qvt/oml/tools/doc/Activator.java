7cbaedd4b6bbfa5adccd8c5c28d259c27be1f463
Ed Willink <ed@willink.me.uk>
2018-12-05 14:58:52 +0000

    [releng] Apply copyright tool to QVTo tests

--- doc/org.eclipse.m2m.qvt.oml.tools.doc/src/org/eclipse/m2m/qvt/oml/tools/doc/Activator.java
+++ doc/org.eclipse.m2m.qvt.oml.tools.doc/src/org/eclipse/m2m/qvt/oml/tools/doc/Activator.java
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (c) 2014,2015 ASML Netherlands B.V. and others.
+ * Copyright (c) 2014,2018 ASML Netherlands B.V. and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
